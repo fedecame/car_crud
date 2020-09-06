5fc97230f3bf7a4e4b67f6443d1866e9bb647719
fedecame <fede.cameriere@gmail.com>
2020-09-05 21:30:38 -0300

    added sass and removed extra create-react-app features

--- car_crud_frontend/src/App.js
+++ car_crud_frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -19,6 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Button variant="dark"> Test </Button>
+
+      </div>
     </div>
   );
 }
